945049eadcf106332be273af872c4a4b43008fd0
Alexey <alekseyalekseyev71@gmail.com>
2026-08-16 19:35:43 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_PAGES ? '/sveta-alex/' : './',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
